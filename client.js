87f098f87f98041abd30daa4f0e4b686798a074d
DillanChung <144236526+DillanChung@users.noreply.github.com>
2024-04-30 19:52:10 -0400

    Updated repo

--- client.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const URL = "https://cgwybapyhlmgmkihomfj.supabase.co";
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNnd3liYXB5aGxtZ21raWhvbWZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ1MTY5NTcsImV4cCI6MjAzMDA5Mjk1N30.Gfi51PSuAySJ7Nw7hnKyNJbRq5aamapdf7bALhoZOGA";
-const supabase = createClient(URL, API_KEY);
-
-export { supabase };
